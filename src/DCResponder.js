ac1182f341f217af7cbee8b652d3aabed08202d1
Kenneth Chew <kenneth.c0@protonmail.com>
2021-04-17 01:16:08 -0400

    Refactor code
    Move logic for rules and message matching into a separate class

--- src/DCResponder.js
+++ src/DCResponder.js
@@ -1,41 +1,29 @@
 // TODO: the config should probably be checked for validity
 const config = require('../config.json');
+let rules;
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const RespondRule = require('./RespondRule.js')
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    rules = config.usersToRespondTo.map(rule => {
+        return RespondRule.from(rule);
+    })
 });
 
 client.on("message", message => {
     // only respond to first matching event
     let isComplete = false;
-    config.usersToRespondTo.forEach(whitelistedUser => {
+
+    rules.forEach(rule => {
         if (isComplete) return;
-        if (whitelistedUser.userID !== undefined && !(whitelistedUser.userID === message.author.id.toString())) return;
 
-        let messageMatched = false;
-        if (!messageMatched && whitelistedUser.receivedMessageCaseInsensitive !== undefined) {
-            messageMatched = whitelistedUser.receivedMessageCaseInsensitive.toLowerCase() === message.content.toLowerCase();
-            let currentEmbed = 0;
-            while (!messageMatched && currentEmbed < message.embeds.length) {
-                messageMatched = whitelistedUser.receivedMessageCaseInsensitive.toLowerCase() === message.embeds[currentEmbed].description.toLowerCase();
-                currentEmbed++;
-            }
-        }
-        if (!messageMatched && whitelistedUser.receivedMessageContains !== undefined) {
-            messageMatched = message.content.toLowerCase().indexOf(whitelistedUser.receivedMessageContains.toLowerCase()) !== -1;
-            let currentEmbed = 0;
-            while (!messageMatched && currentEmbed < message.embeds.length) {
-                messageMatched = message.embeds[currentEmbed].description.toLowerCase().indexOf(whitelistedUser.receivedMessageContains.toLowerCase()) !== -1;
-                currentEmbed++;
-            }
-        }
-
-        if (messageMatched) {
+        if (rule.matches(message)) {
             isComplete = true;
-            message.channel.send(whitelistedUser.sendMessage);
+            message.channel.send(rule.sendMessage);
         }
     })
 })
---
+++ src/RespondRule.js
@@ -0,0 +1,62 @@
+class RespondRule {
+    /**
+     * Creates a blank `RespondRule` that matches no messages.
+     */
+    constructor() {
+        /**
+         * @type {String}
+         */
+        this.userID = null;
+        /**
+         * @type {String}
+         */
+        this.receivedMessageCaseInsensitive = null;
+        /**
+         * @type {String}
+         */
+        this.receivedMessageContains = null;
+        /**
+         * @type {String}
+         */
+        this.sendMessage = null;
+    }
+
+    /**
+     * Creates a new `RespondRule` from the given `json` data.
+     * @param json
+     * @returns {RespondRule}
+     */
+    static from(json) {
+        return Object.assign(new RespondRule(), json);
+    }
+
+    /**
+     * Determines if the given `message` matches this rule.
+     * @param message
+     * @returns {boolean} `true` if the given `message` matches this rule, otherwise `false`.
+     */
+    matches(message) {
+        if (this.userID !== null && this.userID !== message.author.id.toString()) return false;
+
+        let messageMatched = false;
+        if (!messageMatched && this.receivedMessageCaseInsensitive !== null) {
+            messageMatched = this.receivedMessageCaseInsensitive.toLowerCase() === message.content.toLowerCase();
+            let currentEmbed = 0;
+            while (!messageMatched && currentEmbed < message.embeds.length) {
+                messageMatched = this.receivedMessageCaseInsensitive.toLowerCase() === message.embeds[currentEmbed].description.toLowerCase();
+                currentEmbed++;
+            }
+        }
+        if (!messageMatched && this.receivedMessageContains !== null) {
+            messageMatched = message.content.toLowerCase().indexOf(this.receivedMessageContains.toLowerCase()) !== -1;
+            let currentEmbed = 0;
+            while (!messageMatched && currentEmbed < message.embeds.length) {
+                messageMatched = message.embeds[currentEmbed].description.toLowerCase().indexOf(this.receivedMessageContains.toLowerCase()) !== -1;
+                currentEmbed++;
+            }
+        }
+        return messageMatched;
+    }
+}
+
+module.exports = RespondRule
